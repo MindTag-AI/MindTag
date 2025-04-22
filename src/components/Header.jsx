b45df33aebe95bdc1eca79785979cbec02bef77b
MindTag <mindtag1@gmail.com>
2025-04-22 22:47:48 +0100

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>MindTag</h1>
+      <nav>
+        <a href="#features">Features</a>
+        <a href="#roadmap">Roadmap</a>
+      </nav>
+    </header>
+  );
+}
